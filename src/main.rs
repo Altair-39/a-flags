8cfd6c898f817e06a60171e26438cc679d9f223e
altair <luca.barra@edu.unito.it>
2025-02-02 03:18:28 +0100

    update code and version

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use clap::{Arg, Command};
 use colored::*;
+use phf::phf_map;
+use std::fmt;
+use std::str::FromStr;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Flag {
     Rainbow,
     Lesbian,
@@ -25,36 +28,104 @@ enum Flag {
     Trigender,
 }
 
-fn get_flag_palette(flag: Flag) -> Vec<&'static str> {
-    match flag {
-        Flag::Rainbow => vec![
-            "#E40303", "#FF8C00", "#FFED00", "#008026", "#004CFF", "#732982",
-        ],
-        Flag::Lesbian => vec!["#D62E02", "#FD9855", "#FFFFFF", "#D161A2", "#A20160"],
-        Flag::Gay => vec![
-            "#078D70", "#26CEAA", "#98E8C1", "#FFFFFF", "#7BADE2", "#5049CC", "#3D1A78",
-        ],
-        Flag::Bisexual => vec!["#D60270", "#D60270", "#9B4F96", "#0038A8", "#0038A8"],
-        Flag::Transgender => vec!["#55CDFC", "#F7A8B8", "#FFFFFF", "#F7A8B8", "#55CDFC"],
-        Flag::Asexual => vec!["#000000", "#A4A4A4", "#FFFFFF", "#800080"],
-        Flag::Pansexual => vec!["#FF218C", "#FFD800", "#21B1FF"],
-        Flag::NonBinary => vec!["#FFF430", "#FFFFFF", "#9C59D1", "#000000"],
-        Flag::GenderQueer => vec!["#B57EDC", "#FFFFFF", "#4A8123"],
-        Flag::Mlm => vec!["#78D70D", "#3CB371", "#FFFFFF", "#3CB371", "#78D70D"],
-        Flag::Aromantic => vec!["#3DA542", "#A7D379", "#FFFFFF", "#A9A9A9", "#000000"],
-        Flag::Polysexual => vec!["#F61CB9", "#07D569", "#1C92F6"],
-        Flag::Demiboy => vec!["#7F7FFF", "#C4C4C4", "#FFFFFF", "#C4C4C4", "#7F7FFF"],
-        Flag::Demigirl => vec!["#FC9EBB", "#C4C4C4", "#FFFFFF", "#C4C4C4", "#FC9EBB"],
-        Flag::Agender => vec![
-            "#000000", "#B9B9B9", "#FFFFFF", "#B7F684", "#FFFFFF", "#B9B9B9", "#000000",
-        ],
-        Flag::Bigender => vec!["#C479A2", "#F9CEDF", "#FFFFFF", "#F9CEDF", "#C479A2"],
-        Flag::Genderfluid => vec!["#FF75A2", "#FFFFFF", "#BD88F5", "#000000", "#333EBD"],
-        Flag::Abrosexual => vec!["#75EAB6", "#ADF784", "#FFFFFF", "#F78BB3", "#F12D6F"],
-        Flag::Neutrois => vec!["#FFFFFF", "#4DDC82", "#000000"],
-        Flag::Trigender => vec!["#F6AAB7", "#DAA8E9", "#A3E3F4", "#DAA8E9", "#F6AAB7"],
+impl Flag {
+    fn as_str(&self) -> &'static str {
+        match self {
+            Flag::Rainbow => "rainbow",
+            Flag::Lesbian => "lesbian",
+            Flag::Gay => "gay",
+            Flag::Bisexual => "bisexual",
+            Flag::Transgender => "transgender",
+            Flag::Asexual => "asexual",
+            Flag::Pansexual => "pansexual",
+            Flag::NonBinary => "nonbinary",
+            Flag::GenderQueer => "genderqueer",
+            Flag::Mlm => "mlm",
+            Flag::Aromantic => "aromantic",
+            Flag::Polysexual => "polysexual",
+            Flag::Demiboy => "demiboy",
+            Flag::Demigirl => "demigirl",
+            Flag::Agender => "agender",
+            Flag::Bigender => "bigender",
+            Flag::Genderfluid => "genderfluid",
+            Flag::Abrosexual => "abrosexual",
+            Flag::Neutrois => "neutrois",
+            Flag::Trigender => "trigender",
+        }
     }
 }
+
+impl FromStr for Flag {
+    type Err = FlagParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "rainbow" => Ok(Flag::Rainbow),
+            "lesbian" => Ok(Flag::Lesbian),
+            "gay" => Ok(Flag::Gay),
+            "bisexual" => Ok(Flag::Bisexual),
+            "transgender" => Ok(Flag::Transgender),
+            "asexual" => Ok(Flag::Asexual),
+            "pansexual" => Ok(Flag::Pansexual),
+            "nonbinary" => Ok(Flag::NonBinary),
+            "genderqueer" => Ok(Flag::GenderQueer),
+            "mlm" => Ok(Flag::Mlm),
+            "aromantic" => Ok(Flag::Aromantic),
+            "polysexual" => Ok(Flag::Polysexual),
+            "demiboy" => Ok(Flag::Demiboy),
+            "demigirl" => Ok(Flag::Demigirl),
+            "agender" => Ok(Flag::Agender),
+            "bigender" => Ok(Flag::Bigender),
+            "genderfluid" => Ok(Flag::Genderfluid),
+            "abrosexual" => Ok(Flag::Abrosexual),
+            "neutrois" => Ok(Flag::Neutrois),
+            "trigender" => Ok(Flag::Trigender),
+            _ => Err(FlagParseError),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct FlagParseError;
+
+impl fmt::Display for FlagParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Invalid flag name")
+    }
+}
+
+impl std::error::Error for FlagParseError {}
+
+static FLAG_PALETTES: phf::Map<&'static str, &[&str]> = phf_map! {
+    "rainbow" => &["#E40303", "#FF8C00", "#FFED00", "#008026", "#004CFF", "#732982"],
+    "lesbian" => &["#D62E02", "#FD9855", "#FFFFFF", "#D161A2", "#A20160"],
+    "gay" => &["#078D70", "#26CEAA", "#98E8C1", "#FFFFFF", "#7BADE2", "#5049CC", "#3D1A78"],
+    "bisexual" => &["#D60270", "#D60270", "#9B4F96", "#0038A8", "#0038A8"],
+    "transgender" => &["#55CDFC", "#F7A8B8", "#FFFFFF", "#F7A8B8", "#55CDFC"],
+    "asexual" => &["#000000", "#A4A4A4", "#FFFFFF", "#800080"],
+    "pansexual" => &["#FF218C", "#FFD800", "#21B1FF"],
+    "nonbinary" => &["#FFF430", "#FFFFFF", "#9C59D1", "#000000"],
+    "genderqueer" => &["#B57EDC", "#FFFFFF", "#4A8123"],
+    "mlm" => &["#78D70D", "#3CB371", "#FFFFFF", "#3CB371", "#78D70D"],
+    "aromantic" => &["#3DA542", "#A7D379", "#FFFFFF", "#A9A9A9", "#000000"],
+    "polysexual" => &["#F61CB9", "#07D569", "#1C92F6"],
+    "demiboy" => &["#7F7FFF", "#C4C4C4", "#FFFFFF", "#C4C4C4", "#7F7FFF"],
+    "demigirl" => &["#FC9EBB", "#C4C4C4", "#FFFFFF", "#C4C4C4", "#FC9EBB"],
+    "agender" => &["#000000", "#B9B9B9", "#FFFFFF", "#B7F684", "#FFFFFF", "#B9B9B9", "#000000"],
+    "bigender" => &["#C479A2", "#F9CEDF", "#FFFFFF", "#F9CEDF", "#C479A2"],
+    "genderfluid" => &["#FF75A2", "#FFFFFF", "#BD88F5", "#000000", "#333EBD"],
+    "abrosexual" => &["#75EAB6", "#ADF784", "#FFFFFF", "#F78BB3", "#F12D6F"],
+    "neutrois" => &["#FFFFFF", "#4DDC82", "#000000"],
+    "trigender" => &["#F6AAB7", "#DAA8E9", "#A3E3F4", "#DAA8E9", "#F6AAB7"],
+};
+
+fn get_flag_palette(flag: Flag) -> &'static [&'static str] {
+    FLAG_PALETTES
+        .get(flag.as_str())
+        .copied()
+        .unwrap_or(&["#FFFFFF"]) // Default to white
+}
+
 fn main() {
     let matches = Command::new("LGBT Flags")
         .version("1.0")
@@ -91,40 +162,16 @@ fn main() {
         .get_matches();
 
     let flag_str = matches.get_one::<String>("flag").unwrap();
-    let flag = match flag_str.as_str() {
-        "rainbow" => Flag::Rainbow,
-        "lesbian" => Flag::Lesbian,
-        "gay" => Flag::Gay,
-        "bisexual" => Flag::Bisexual,
-        "transgender" => Flag::Transgender,
-        "asexual" => Flag::Asexual,
-        "pansexual" => Flag::Pansexual,
-        "nonbinary" => Flag::NonBinary,
-        "genderqueer" => Flag::GenderQueer,
-        "mlm" => Flag::Mlm,
-        "aromantic" => Flag::Aromantic,
-        "polysexual" => Flag::Polysexual,
-        "demiboy" => Flag::Demiboy,
-        "demigirl" => Flag::Demigirl,
-        "agender" => Flag::Agender,
-        "bigender" => Flag::Bigender,
-        "genderfluid" => Flag::Genderfluid,
-        "abrosexual" => Flag::Abrosexual,
-        "neutrois" => Flag::Neutrois,
-        "trigender" => Flag::Trigender,
-        _ => Flag::Rainbow, // Default to Raibow flag
-                            //
-    };
+    let flag = flag_str.parse::<Flag>().unwrap_or(Flag::Rainbow);
 
     let palette = get_flag_palette(flag);
     for color in palette {
-        println!(
-            "{}",
-            " ".repeat(50).on_truecolor(
-                u8::from_str_radix(&color[1..3], 16).unwrap(),
-                u8::from_str_radix(&color[3..5], 16).unwrap(),
-                u8::from_str_radix(&color[5..7], 16).unwrap()
-            )
+        let (r, g, b) = (
+            u8::from_str_radix(&color[1..3], 16).unwrap(),
+            u8::from_str_radix(&color[3..5], 16).unwrap(),
+            u8::from_str_radix(&color[5..7], 16).unwrap(),
         );
+
+        println!("{}", " ".repeat(20).on_truecolor(r, g, b));
     }
 }
